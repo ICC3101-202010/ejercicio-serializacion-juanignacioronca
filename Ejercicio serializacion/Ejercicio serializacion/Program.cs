db9f504b3898681b4cde7256f01c6ffdbb6eed20
juan ignacio roncagliolo <juanignacioroncagliolo@hotmail.com>
2020-05-07 10:20:24 -0400

    Ejercicio Serializacion Completo Con listas

--- Ejercicio serializacion/Ejercicio serializacion/Program.cs
+++ Ejercicio serializacion/Ejercicio serializacion/Program.cs
@@ -15,8 +15,10 @@ namespace Ejercicio_serializacion
         {
             List<Persona> Lista_Personas = new List<Persona>();
             int a_1 = -1;
+            
             while (a_1 != 0)
             {
+                int i_2 = 0;
                 Console.WriteLine("Bienvenido a creador de personas");
                 Console.WriteLine("\n1.Para crear una nueva persona\n2. Para mostrar la lista\n3. Para cargar los datos\n4. Para descargar datos\n0. Para salir");
                 a_1 = Convert.ToInt32(Console.ReadLine());
@@ -42,22 +44,36 @@ namespace Ejercicio_serializacion
                 else if (a_1 == 3)
                 {
                     IFormatter formatter = new BinaryFormatter();
-                    Stream stream = new FileStream("Lista_Personas.bin", FileMode.Create, FileAccess.Write, FileShare.None);
+                    int i_1 = 0;
                     foreach (var item in Lista_Personas)
                     {
+                        Stream stream = new FileStream("Lista_Personas"+i_1+".bin", FileMode.Create, FileAccess.Write, FileShare.None);
                         formatter.Serialize(stream,item);
+                        i_1++;
+                        stream.Close();
                     }
-                    stream.Close();
                     Console.WriteLine("Serializados correctamente");
 
                 }
-                else if (a_1 == 4)
+                else if (a_1 == 4) //Metodo descargar datos.
                 {
                     IFormatter formatter = new BinaryFormatter();
-                    Stream stream = new FileStream("Lista_Personas.bin", FileMode.Open, FileAccess.Read, FileShare.Read);
-                   
-                    Persona Personas1 = (Persona)formatter.Deserialize(stream);
-                    Lista_Personas.Add(Personas1);
+                    for (int i = 0; i < 100; i++)
+                    {
+                        try
+                        {
+                            Stream stream = new FileStream("Lista_Personas" + i + ".bin", FileMode.Open, FileAccess.Read, FileShare.Read);
+                            Persona persona = (Persona)formatter.Deserialize(stream);
+                            Lista_Personas.Add(persona);
+                        }
+                        catch (Exception)
+                        {
+                            break;
+                            throw;
+                        }
+                        
+                        
+                    }
                     Console.WriteLine("Datos descargados correctamente");
                 }
                 
